d380f43db40d771d13778d7a5824faa014047090
Ronaldo Baker <r.remario.baker@gmail.com>
2025-03-20 11:25:35 +0000

    feat: try new model

--- src/models.py
+++ src/models.py
@@ -13,3 +13,25 @@ class LSTM(nn.Module):
         output, _ = self.lstm(x)
         output = self.fc(output)
         return output
+
+
+class LSTMHiddenSummation(nn.Module):
+    def __init__(self, in_dim: int, hidden_size: int, out_dim: int, num_layers: int, dropout: float) -> None:
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=in_dim, hidden_size=hidden_size, num_layers=num_layers, dropout=dropout, batch_first=True)
+        self.fc= nn.Linear(hidden_size, out_dim)
+
+    def forward(self, x):
+        output, (hidden, cell) = self.lstm(x)
+
+        # Take the last hidden state
+        hidden = hidden[-1].unsqueeze(1) # (batch, 1, hidden_size)
+        hidden = hidden.expand(-1, output.shape[1], -1) # (batch, seq_len, hidden_size)
+
+        cell = cell[-1].unsqueeze(1) # (batch, 1, hidden_size)
+        cell = cell.expand(-1, output.shape[1], -1) # (batch, seq_len, hidden_size) 
+
+        fused = output + hidden + cell
+        # Pass the hidden state summed with the output to the fully connected layer
+        output = self.fc(fused)
+        return output
