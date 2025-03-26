307b3e11f571b75f71dd86329472bcd9ee353d31
Ronaldo Baker <r.remario.baker@gmail.com>
2025-03-26 12:35:27 +0000

    refactor: move encoder model to general models file

--- src/models.py
+++ src/models.py
@@ -65,3 +65,37 @@ class CNN_LSTM(nn.Module):
         output, _ = self.lstm(output)
         output = self.linear(output)
         return output
+
+
+class AHREncoder(nn.Module):
+    def __init__(self, num_features=1, num_classes=1):
+        super(AHREncoder, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels=num_features, out_channels=3, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv1d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
+        self.global_max_pool = nn.AdaptiveMaxPool1d(1)  # Global Average Pooling
+        self.classification = nn.Linear(16, num_classes)  # Final classification/regression layer
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+        self.initialize_weights()
+
+    def forward(self, x):
+        out = x.permute(0, 2, 1)  # Change shape from (batch, seq_len, features) to (batch, features, seq_len)
+        out = self.conv1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.relu(out)
+        out = self.global_avg_pool(out)  # Output shape: (batch, 64, 1)
+        features = out.squeeze(-1)  # Remove the last dimension -> (batch, 64)
+
+        out = self.classification(features)  # Final output
+        preds = self.sigmoid(out)
+        return preds, features
+    
+    def initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d):
+                nn.init.xavier_normal_(m.weight)
+                nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                nn.init.zeros_(m.bias)
