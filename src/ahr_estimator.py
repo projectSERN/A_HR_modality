1fdccdabedabb87f6eafaff31333385271fe26d2
Ronaldo Baker <r.remario.baker@gmail.com>
2025-04-13 10:34:39 +0100

    feat: adjust estimator to handle clip-based inference

--- src/ahr_estimator.py
+++ src/ahr_estimator.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import torch
+import numpy as np
 
 # Append project root to sys.path
 project_root = os.path.join(os.path.dirname(__file__), "..")
@@ -25,15 +26,27 @@ class AHREstimator:
         self.model = self.model.to(self.device)
         self.model.eval()
 
-    def estimate_hr(self, path: str):
+
+    def estimate_hr(self, path: str, clip_length: int = None):
         # Extract MFCCs from video or audio path
         mfccs = self.extractor.feature_extraction(path)
 
         # Transform into tensor
         mfccs_tensor = torch.tensor(mfccs, dtype=torch.float32).unsqueeze(0).to(self.device) # add batch dimension
 
-        # Estimate HR
-        with torch.no_grad():
-            hr = self.model(mfccs_tensor).squeeze(0).detach().cpu().numpy()
+        # If clip_length is not provided, use the entire sequence
+        if clip_length: 
+            mfccs_tensors = torch.split(mfccs_tensor, clip_length, dim=1)
+            mfccs_tensors = [tensor for tensor in mfccs_tensors if tensor.shape[1] == clip_length]
 
-        return hr
+        # Estimate HR
+        hr_values = []
+        with torch.no_grad():
+            if clip_length:
+                for tensor in mfccs_tensors:
+                    hr = self.model(tensor).squeeze(0).detach().cpu().numpy()
+                    hr_values.append(hr)
+                return hr_values
+            else:
+                hr = self.model(mfccs_tensor).squeeze(0).detach().cpu().numpy()
+                return hr
